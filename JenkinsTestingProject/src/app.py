7571355992cda81e354b40e25cac80f1d2c7407a
Srijeet Chatterjee <champ.srijeet@gmail.com>
2020-08-02 01:29:16 +0530

    updated for medium article

--- JenkinsTestingProject/src/app.py
+++ JenkinsTestingProject/src/app.py
@@ -6,11 +6,11 @@ app = flask.Flask(__name__)
 @app.route('/')
 @app.route('/hello/')
 def hello_world():
-    return 'Hello into the world of DevOps!\n'
+    return 'Hello World!\n'
 
 @app.route('/hello/<username>') # dynamic route
 def hello_user(username):
-    return 'Srijeet welcomes you my friend %s!\n' % username
+    return 'I have been Updated my friend %s!\n' % username
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')     # open for everyone
