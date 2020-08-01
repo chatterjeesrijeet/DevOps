d169c9588fd52aca1d55f7a6e4821e4a91545f41
Srijeet Chatterjee <champ.srijeet@gmail.com>
2020-08-01 20:13:05 +0530

    Jenkins Testing Folder added

---
+++ JenkinsTestingProject/src/app.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+import flask
+
+app = flask.Flask(__name__)
+
+@app.route('/')
+@app.route('/hello/')
+def hello_world():
+    return 'Hello into the world of DevOps!\n'
+
+@app.route('/hello/<username>') # dynamic route
+def hello_user(username):
+    return 'Srijeet welcomes you my friend %s!\n' % username
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')     # open for everyone
